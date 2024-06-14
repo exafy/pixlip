6c6e2f9e5cf0fc9340ea916ca5081a94f8ce1a1d
Muhammad Ahsan <csahsan0021@gmail.com>
2024-06-14 22:04:25 +0500

    update for send message widget

--- src/Feature/components/typing-area.tsx
+++ src/Feature/components/typing-area.tsx
@@ -1,3 +1,70 @@
+import styled from "@emotion/styled";
+import { Icon } from "./icon";
+
 export const TypingArea = () => {
-  return <></>;
+  return (
+    <StyledSendMessageContainer>
+      <IconsContainer>
+        <Icon size="xx-large" name="photo_camera" />
+        <Icon size="xx-large" name="image" />
+        <Icon size="xx-large" name="description" />
+      </IconsContainer>
+      <StyledCardContainer>
+        <TypingAreaContainer placeholder="Type here or click mic to talk with me..." />
+        <Icon name="send" onClick={() => {}} />
+      </StyledCardContainer>
+      <Icon size="xx-large" name="mic" />
+    </StyledSendMessageContainer>
+  );
 };
+
+const StyledSendMessageContainer = styled.div`
+  display: flex;
+  padding-left: 175px;
+  padding-right: 175px;
+  height: 100px;
+  align-items: center;
+  gap: 10px;
+  position: fixed;
+  bottom: 20px;
+  width: calc(100vw - 600px);
+  justify-content: space-between;
+`;
+
+const IconsContainer = styled.div`
+  display: flex;
+`;
+
+const TypingAreaContainer = styled.input`
+  height: inherit;
+  color: #000000;
+  font-family: Roboto;
+  width: 100%;
+  border: none;
+  outline: none;
+  border-radius: 20px;
+  font-size: 16px;
+  font-weight: 100;
+  line-height: normal;
+  letter-spacing: 0.16px;
+  background: transparent;
+  transition: 0.4s;
+  resize: none;
+  overflow: hidden;
+  position: relative;
+  &::placeholder {
+    color: #9f9f9f;
+    font-style: italic;
+    font-weight: 400;
+  }
+`;
+
+const StyledCardContainer = styled.div`
+  border-radius: 30px;
+  border: 2px solid #000;
+  height: 20px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding: 20px;
+`;
--- src/Feature/page/conversations-list-page.tsx
+++ src/Feature/page/conversations-list-page.tsx
@@ -1,13 +1,20 @@
 import styled from "@emotion/styled";
 import { useParams } from "react-router-dom";
 import { Sidebar } from "../components/chat-sidebar/chat-side";
+import { TypingArea } from "../components/typing-area";
 
 export const ConversationListPage = () => {
   const { id } = useParams();
   const conversationId = parseInt(id as string);
-    return <StyledConversationLayout>
+  return (
+    <StyledConversationLayout>
       <Sidebar></Sidebar>
-  </StyledConversationLayout>;
+      <StyledChatViewContainer></StyledChatViewContainer>
+      <StyledContentArea>
+        <TypingArea />
+      </StyledContentArea>
+    </StyledConversationLayout>
+  );
 };
 
 const StyledConversationLayout = styled.div`
@@ -16,4 +23,12 @@ const StyledConversationLayout = styled.div`
 const StyledContentArea = styled.div`
   display: flex;
   width: calc(100vw - 278px);
+`;
+
+const StyledChatViewContainer = styled.div`
+  height: calc(100vh - 120px);
+  display: flex;
+  width: calc(100vw - 250px);
+  right: 0;
+  position: absolute;
 `;
